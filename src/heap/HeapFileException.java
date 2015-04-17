a046abd87d439923194a8cca7169c6fb06b1870b
centauroWaRRIor <emmanuel.villagomez@gmail.com>
2015-04-16 23:55:47 -0500

    creating part4 branch

--- src/heap/HeapFileException.java
+++
@@ -1,9 +0,0 @@
-package heap;
-
-import chainexception.ChainException;
-
-public class HeapFileException extends ChainException {
-    public HeapFileException(Exception e, String s) {
-        super(e,s);
-    }
-}
